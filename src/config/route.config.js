098f7771a2cabe1c086773c871342868bdab1681
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-12-01 00:04:18 +0330

    feat : add router

---
+++ src/config/route.config.js
@@ -0,0 +1,13 @@
+export const PATHS = {
+  HOME: "/",
+  PRODUCTS: "products",
+  PRODUCT: "products/product", //:productId
+  BASKET: "basket",
+  CHECKOUT: "/checkout",
+  NOTFOUND: "*",
+  LOGIN: "login",
+  PANELADMIN: "/",
+  PANELPRODUCTS: "/admin",
+  PANELORDERS: "panelOrders",
+  PANELQUANTITY: "panelQuantity",
+};
