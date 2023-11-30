e0f17a4d94266d7dc7f4bea9c8c070be40da7cc7
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-11-30 23:40:54 +0330

    feat : add components pagination

---
+++ src/components/common/pagination/MainPagination.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MainPagination = () => {
+  return <div>MainPagination</div>;
+};
---
+++ src/components/common/pagination/Next.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Next = () => {
+  return <div>Next</div>;
+};
