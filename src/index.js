30bec137c2aa2cbbe1e3305fc213d33b5d5702f2
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-12-07 23:47:03 +0330

    feat:add query client provider with query client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import "tw-elements-react/dist/css/tw-elements-react.min.css";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// ******************************
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+
+import "./index.css";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
