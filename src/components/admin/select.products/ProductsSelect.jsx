d02fa45ffe3574917f9db6fd257485f255f2a58f
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-12-07 23:35:31 +0330

    feat:set config api

---
+++ src/components/admin/select.products/ProductsSelect.jsx
@@ -0,0 +1,34 @@
+import { GetCategories } from "../../../services";
+
+export const ProductsSelect = ({ onCategoryChange, data }) => {
+  const arrayOfCategory = [...GetCategories()] || [];
+
+  const handleCategoryChange = (e) => {
+    const categoryId = e.target.value;
+    onCategoryChange(categoryId);
+  };
+
+  return (
+    <label>
+      <select
+        value={data}
+        className="bg-transparent cursor-pointer outline-0"
+        name="selectedCategory"
+        onChange={handleCategoryChange}
+      >
+        <option value="all" className="bg-[#739072]">
+          تمام دسته بندی ها
+        </option>
+        {arrayOfCategory.map((category, index) => (
+          <option
+            className="bg-[#739072]"
+            key={category.id || index}
+            value={category.id}
+          >
+            {category.name}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,6 @@
-export const BASE_URL = "http://localhost:8000/api";
-export const PRODUCTS_URL = "/products";
-export const ORDERS_URL = "/orders";
-export const LOGIN_URL = "/auth/login";
+export const BASE_URL = "http://localhost:8000/api/";
+export const CATEGORIES_URL = "categories";
+export const PRODUCTS_URL = "products";
+export const ORDERS_URL = "orders";
+export const USERS_URL = "users";
+export const SUBCATEGORIES_URL = "subcategories";
