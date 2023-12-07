5e85e5c7c6d3f089f651bc75bcd8c907d26acee1
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-12-07 23:09:01 +0330

    feat:add pagination

--- src/components/common/pagination/MainPagination.jsx
+++ src/components/common/pagination/MainPagination.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import { Previous } from "./Previous";
+import { PaginationNumber } from "./PaginationNumber";
+import { Next } from "./Next";
+export const MainPagination = ({ totalPages, currentPage, onPageChange }) => {
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
 
-export const MainPagination = () => {
-  return <div>MainPagination</div>;
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+  console.log("paginate");
+  const createArray = () => {
+    let newArray = [];
+    for (let i = 1; i <= totalPages; i++) {
+      newArray.push(i);
+    }
+    return newArray;
+  };
+  createArray();
+  console.log(totalPages);
+  console.log(createArray());
+  return (
+    <>
+      <nav>
+        <ul className="list-style-none flex justify-center items-center">
+          <Previous currentPage={currentPage} onPrevPage={handlePrevClick} />
+          {createArray().map((page) => (
+            <PaginationNumber
+              key={page}
+              page={page}
+              label={page}
+              onPageChange={onPageChange}
+              currentPage={currentPage}
+            />
+          ))}
+          <Next
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onNextPage={handleNextClick}
+          />
+        </ul>
+      </nav>
+    </>
+  );
 };
--- src/components/common/pagination/Next.jsx
+++ src/components/common/pagination/Next.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Next = () => {
-  return <div>Next</div>;
+export const Next = ({ currentPage, totalPages, onNextPage }) => {
+  return (
+    <li>
+      <a
+        className={`${
+          currentPage === totalPages ? "text-neutral-400" : "bg-transparent"
+        } px-3 py-1.5 text-base cursor-pointer duration-300 relative block`}
+        aria-label="Next"
+        onClick={onNextPage}
+      >
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  );
 };
