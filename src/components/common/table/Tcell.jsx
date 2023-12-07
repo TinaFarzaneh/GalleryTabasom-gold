8e763212eacf21775805079b7e2a52ed3398d25a
Tina Ramezanifarzaneh <ramezanift73@gmail.com>
2023-12-07 23:06:32 +0330

    feat:add table ui

--- src/components/common/table/Tcell.jsx
+++ src/components/common/table/Tcell.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-
-export const Tcell = () => {
-  return <div>Tcell</div>;
+export const TCell = ({ title, width, icon }) => {
+  return (
+    <th className={`${width} px-6 py-4 text-right`}>
+      <div className="flex items-center gap-1">
+        <div className="cursor-pointer">{icon}</div>
+        {title}
+      </div>
+    </th>
+  );
 };
--- src/components/common/table/Thead.jsx
+++ src/components/common/table/Thead.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { TCell } from "./Tcell";
 
-export const Thead = () => {
-  return <div>Thead</div>;
+export const THead = ({ columns = [] }) => {
+  console.log(columns);
+  return (
+    <thead className="border-b font-medium bg-[#739072] shadow-sm">
+      <tr>
+        {columns.map((col) => (
+          <TCell
+            key={col.id}
+            title={col.title}
+            icon={col.icon}
+            width={col.width}
+          />
+        ))}
+      </tr>
+    </thead>
+  );
 };
